e4bef1d4d3d0e766645730d2e15295bd57183b0b
gshorti <ilazajkin52969@gmail.com>
2024-08-27 14:53:22 +0300

    Сделан поиск по имени видео и описанию.

--- KpTube/video/views.py
+++ KpTube/video/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
 from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.response import Response
@@ -11,5 +12,6 @@ from video.sserializers import VideoSerializer
 class VideoViewSet(ModelViewSet):
     queryset = Video.objects.all()
     serializer_class = VideoSerializer
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend, SearchFilter]
     filterset_fields = ['category']
+    search_fields = ['name', 'description']
