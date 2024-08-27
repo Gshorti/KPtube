5d0a93de69dfd271604e304d50f2671159d1cfb7
gshorti <ilazajkin52969@gmail.com>
2024-08-27 14:27:30 +0300

    Добавил категории

--- KpTube/KpTube/settings.py
+++ KpTube/KpTube/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'video',
     'rest_framework',
     'corsheaders',
+    'category',
 ]
 
 MIDDLEWARE = [
--- KpTube/video/models.py
+++ KpTube/video/models.py
@@ -5,7 +5,10 @@ from django.db import models
 class Video(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
+    likes = models.IntegerField(default=0)
+    views = models.IntegerField(default=0)
     video = models.FileField(upload_to='videos/')
+    category = models.ForeignKey('category.Category', on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.name
